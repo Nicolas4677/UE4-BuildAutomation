5ae5c1f2cd7d45b26eefd1326638ca88af4a2f98
Nicolas Morales Escobar <pg18nicolas@vfs.com>
2020-08-01 15:48:46 -0700

    Automated Build

---
+++ Commands/UEAutomatedBuild.cmd
@@ -0,0 +1,30 @@
+@echo off
+
+REM Copyright (C) 2020. Nicolas Morales Escobar. All rights reserved.
+
+REM This is the folder where the project and the build will be created
+set basePath="C:\Users\student\Desktop\Nicolas\Term4\Pipelines\BuildAutomation\BuildTest"
+
+REM This is the folder where the RunUAT.bat is.
+set enginePath="C:\Program Files\Epic Games\UE_4.25\Engine\Build\BatchFiles"
+
+cd %basePath%
+
+echo Creating Project folder.
+mkdir Project
+
+echo Creating Build folder.
+mkdir Build
+
+REM You've got to change the branch and the HTTPS/SSH of your specific repository.
+git clone -b develop https://github.com/Nicolas4677/UE4_BuildAutomation.git %basePath%\Project
+
+cd %enginePath%
+
+REM You also have to change the name of the UE project to your specific project.
+RunUAT BuildCookRun -project="%basePath%\Project\BuildAutomationTest\BuildAutomationTest.uproject" -noP4 -platform=Win64 -clientconfig=Development -serverconfig=Development -cook -allmaps -build -stage -pak -archive -archivedirectory="%basePath%\Build"
+
+echo Deleting Project folder which is no longer needed.
+rd /s /q "%basePath%\Project"
+
+echo Build is located in: %basePath%\Build
